ba79babba2093b21659f40167db8d14f6164b120
Photekk <Junglist328>
2014-08-18 10:04:21 -0400

    Initial Commit

---
+++ ParentMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParentMobile
+//
+//  Created by Justin Owens on 8/18/14.
+//  Copyright (c) 2014 VisionCPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParentMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParentMobile
+//
+//  Created by Justin Owens on 8/18/14.
+//  Copyright (c) 2014 VisionCPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
